d73c00ad75b208ece7c6c10a4e5b24bbb8afba98
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2020-07-29 17:20:33 -0700

    Update CIP-LinkedList_Traversal.py

--- CIP-LinkedList_Traversal.py
+++ CIP-LinkedList_Traversal.py
@@ -2,11 +2,12 @@
 #create a function to copy a singly linked list, also what is the bigO
 #take that singly linked list and let it have a random pointer (like next but it can point to any node in that list or itself or be null)
 #create a function to copy that linked list including the random pointer.  what is the bigO
-
+import random
 class Node:
      def __init__(self,initdata=""):
           self.data = initdata
           self.next = None
+          self.randNext = None
      def getData(self):
           return self.data
      def getNext(self):
@@ -15,6 +16,8 @@ class Node:
           self.data = newdata
      def setNext(self,newnext):
           self.next = newnext
+     def getRandNext(self):
+          return self.randNext
           
 class LinkList:
 	def __init__(self):
@@ -26,11 +29,11 @@ class LinkList:
 			return "empty"
 		
 		current = self.head
-		mystring = str (current.getData() )
+		mystring = str (current )
 		while current.getNext() != None:
 			current = current.getNext()
 			mystring +=" -> "
-			mystring += str(current.getData())
+			mystring += str(current)
 		return mystring
 	def add(self,item):
 		temp = Node(item)    
@@ -47,29 +50,51 @@ class LinkList:
 				while current.getNext() != None:    # here is that test again
 					current = current.getNext()
 				self.size += 1 ##CHANGE
+
 	def findNode(self, index,current=None):
-		if self.size == 0:
-			return False
 		current = self.head
 		tempIndex = 0
-		while tempIndex != index:
+		while tempIndex != index and current != None:
 			current = current.getNext()
 			tempIndex += 1
-		return current.getData()
+		return current
+	def setRandNext(self,node):
+		randNum = random.randint(0,self.size)
+		if randNum == self.size:
+			node.randNext = None
+		else:
+			node.randNext = self.findNode(randNum)
+
 def copyList(linkListToCopy):
      linkedListCopy = LinkList()
      current = linkListToCopy.head
      temp = Node(current.getData())
-     temp.setNext(current.getNext())
+     tempNext = current.getNext()
      linkedListCopy.head = temp
-     linkedListCopy.size += linkListToCopy.size
+     linkedListCopy.size += 1
+     while linkedListCopy.size != linkListToCopy.size:
+          temp = Node(current.getData())
+          tempNext = current.getNext()
+          temp.setNext(Node(tempNext.getData()))
+          temp = temp.getNext()
+          current = current.getNext()
+          linkedListCopy.size += 1
+          
+     # what happens when you try to copy an empty list?
+     # what happens when it is a list of size 1?
+     # what happens when you try to copy a list with size 3?
+     # what is the difference between current, temp and tempNext?
+     
      return linkedListCopy
 
 
 listThing = LinkList()
 for i in range(3,15, 2):
 	listThing.add(i)
-	print("current nl is", listThing)
-print(listThing.findNode(3))
+print("current", listThing)
+print(listThing.findNode(3).getData())
 newThing = copyList(listThing)
-print(newThing)
+print("Copy",newThing)
+'''for x in range(0,newThing.size):
+     newThing.setRandNext(newThing.findNode(x))
+     print(newThing.findNode(x))'''
